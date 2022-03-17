1dbe9b5653b524047511c70f1e6ff4a8e0f7ce5f
seungwon-yoo <qwer15417@gmail.com>
2022-03-17 21:52:57 +0900

    fix to send an image file

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ def sayHello():
 
 
 @app.route('/get_image')
-def get_image():
+def get_image1():
     if request.args.get('type') == '1':
         filename = 'corgi.jpg'
     else:
@@ -49,6 +49,12 @@ def get_response_image(image_path):
     return encoded_img
 
 
+@app.route('/images/clothes/<category>/<image_name>', methods=['GET'])
+def get_image(category, image_name):
+    image_path = './clothes/' + category + '/' + image_name
+    return send_file(image_path, mimetype='image/png')
+
+
 @app.route('/settings/init', methods=['GET'])
 def initial_settings():
     categories = ['OUTER', 'DRESS', 'PANTS', 'SKIRT', 'TOP']
@@ -56,15 +62,16 @@ def initial_settings():
     json_data = {
         'clothes': []
     }
-    path_dir = './clothes'
+    path_dir = '/clothes'
     for category in categories:
-        detail_path_dir = path_dir + '/' + category
+        detail_path_dir = '.' + path_dir + '/' + category
         file_list = os.listdir(detail_path_dir)
 
         for element_name in file_list:
             data = {'category': category}
-            img_name = detail_path_dir + '/' + element_name
-            data['result'] = get_response_image(img_name)
+            img_name = '/images' + path_dir + '/' + category + '/' + element_name
+            # data['result'] = get_response_image(img_name)
+            data['image'] = img_name
             json_data['clothes'].append(data)
 
     return jsonify(json_data)
